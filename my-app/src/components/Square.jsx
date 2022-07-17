242a10d64f3fb3915c731bbbe020825d6927012d
Wanderson <wandersongamer2@gmail.com>
2022-07-17 01:23:06 -0300

    update

--- my-app/src/components/Square.jsx
+++ my-app/src/components/Square.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className='square'>
-                {  }
-            </button>
-        );
-    }
+// Class ->
+// class Square extends React.Component {
+
+//     render() {
+//         return (
+//             <button 
+//               className='square' 
+//               onClick={() => this.props.onClick()}
+//             >
+//                 { this.props.value }
+//             </button>
+//         );
+//     }
+// }
+
+// Function ->
+// function Square(props) {
+//     return (
+//         <button className='square' onClick={props.onClick}>
+//             {props.value}
+//         </button>
+//     );
+// }
+
+// Arrow Function ->
+const Square = (props) => {
+    return (
+        <button className='square' onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
+
 
 export default Square;
